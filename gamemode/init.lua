3bdf0e84d2c20928724fb249a3fef59550053b6a
FiK <unconfigured@null.spigotmc.org>
2018-09-20 22:51:51 -0700

    Maybe some SWEP stuff?

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -7,3 +7,44 @@ function GM:PlayerSpawn( ply )
   ply:ChatPrint( "You have spawned!" )
   print( "Player: " .. ply:Nick() .. " has spawned!" )
 end
+
+
+SWEP.PrintName = "Inventory"
+SWEP.Purpose = "Manage your Equipment"
+
+SWEP.Slot = 0
+SWEP.SlotPos = 4
+
+SWEP.Spawnable = true
+
+SWEP.ViewModel = Model( "models/weapons/c_arms.mdl" )
+SWEP.WorldModel = ""
+SWEP.ViewModelFOV = 54
+SWEP.UseHands = true
+
+SWEP.Primary.ClipSize = -1
+SWEP.Primary.DefaultClip = -1
+SWEP.Primary.Automatic = true
+SWEP.Primary.Ammo = "none"
+
+SWEP.Secondary.ClipSize = -1
+SWEP.Secondary.DefaultClip = -1
+SWEP.Secondary.Automatic = true
+SWEP.Secondary.Ammo = "none"
+
+SWEP.DrawAmmo = false
+
+SWEP.HitDistance = 48
+
+function SWEP:Initialize()
+
+	self:SetHoldType( "fist" )
+
+end
+
+function SWEP:UpdateNextIdle()
+
+	local vm = self.Owner:GetViewModel()
+	self:SetNextIdle( CurTime() + vm:SequenceDuration() / vm:GetPlaybackRate() )
+
+end
